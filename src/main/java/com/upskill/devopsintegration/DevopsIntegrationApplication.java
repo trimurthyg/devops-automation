4f195e95da76cd4980dbe1cd7b10fc1532c3e3db
Raviteja Gangirddy <raviteja.g0717@gmail.com>
2024-07-30 11:54:13 +0530

    first commit

---
+++ src/main/java/com/upskill/devopsintegration/DevopsIntegrationApplication.java
@@ -0,0 +1,19 @@
+package com.upskill.devopsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DevopsIntegrationApplication {
+
+    @GetMapping
+    public String message() {
+        return "Welcome to Devops Integration";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsIntegrationApplication.class, args);
+    }
+
+}
